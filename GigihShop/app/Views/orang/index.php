65a718a1806f39e41c71bb09f80f624b1aa0a8e2
Catur Saputro <50752647+lolimilkita@users.noreply.github.com>
2021-12-14 23:20:05 +0700

    FirstCommit
    Create New project, CI4, Add Login Register using library Myth/Auth -> https://github.com/lonnieezell/myth-auth
    Add landing page, contact, and barang . But still dummy text

---
+++ GigihShop/app/Views/orang/index.php
@@ -0,0 +1,49 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mb-3 text-center">Daftar Orang</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <form action="" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Masukkan keyword pencarian.." name="keyword">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2" name="submit">Cari</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1 + (6 * ($currentPage - 1)); ?>
+                    <?php foreach($orang as $o) : ?>
+                    <tr>
+                        <th scope="row"><?= $i++; ?></th>
+                        <td><?= $o['nama']; ?></td>
+                        <td><?= $o['alamat']; ?></td>
+                        <td>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?= $pager->links('orang', 'orang_pagination'); ?>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
