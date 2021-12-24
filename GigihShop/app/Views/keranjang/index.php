aa1479f62f56159b484c07c1608ec24c2370194c
Catur Saputro <50752647+lolimilkita@users.noreply.github.com>
2021-12-25 02:10:05 +0700

    Update Keranjang, add method spoofing
    Add http method spoofing in keranjang so that user cant delete from the http even the user logging in on not

--- GigihShop/app/Views/keranjang/index.php
+++ GigihShop/app/Views/keranjang/index.php
@@ -2,38 +2,55 @@
 
 <?= $this->section('content'); ?>
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col">
             <h1 class="mb-3 text-center">Keranjang Saya</h1>
         </div>
-        <div class="row">
-            <div class="col">
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Alamat</th>
-                        <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i = 1 + (6 * ($currentPage - 1)); ?>
-                        <?php foreach($orang as $o) : ?>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td><?= $o['nama']; ?></td>
-                            <td><?= $o['alamat']; ?></td>
-                            <td>
-                                <a href="" class="btn btn-success">Detail</a>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-                <?= $pager->links('orang', 'orang_pagination'); ?>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="card  border-0" id="cardKeranjang">
+                <div class="col table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Gambar</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Total Harga</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = 1; ?>
+                            <?php foreach($keranjang as $k) : ?>
+                            <tr>
+                                <th scope="row"><?= $i++; ?></th>
+                                <td><img src="/img/barang/<?= $k['gambar']; ?>" alt="" class="sampul img-fluid"></td>
+                                <td><?= $k['nama_barang']; ?></td>
+                                <td><?= $k['harga_barang']; ?></td>
+                                <td>
+                                    <a href="/keranjang/detail/<?= $k['nama_barang']; ?>" class="btn btn-success">Detail</a>
+                                    <form action="/keranjang/<?= $k['id']; ?>" method="POST" class="d-inline">
+                                        <?= csrf_field(); ?>
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah anda yakin?');">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+    
 </div>
 <?= $this->endSection(); ?>
